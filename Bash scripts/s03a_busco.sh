37d5cebe10eb4e50f0c9849f20beab8e8a5cd82f
KailunZM <89222331+KailunZM@users.noreply.github.com>
2026-02-05 21:03:10 -0600

    Add files via upload

---
+++ Bash scripts/s03a_busco.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#===============================================================================
+# File Name    : s03a_busco.sh
+# Description  : This script will run amrfinder in parallel
+# Usage        : sbatch s03a_busco.sh
+# Author       : Kailun Zhang, kailun@wustl.edu
+# Version      : 1.2
+# Created On   : Oct 04 2021
+# Last Modified: May 24 2022
+#===============================================================================
+#Submission script for HTCF
+#SBATCH --job-name=busco
+#SBATCH --array=1-11094
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4G
+#SBATCH --output=/scratch/gdlab/kailun/Phageome/NEC/slurm/busco/x_busco_%A_%a.out
+#SBATCH --error=/scratch/gdlab/kailun/Phageome/NEC/slurm/busco/y_busco_%A_%a.out
+
+#activate env
+#conda activate /scratch/ref/gdlab/BUSCO/5.2.2
+
+basedir="/scratch/gdlab/kailun/Phageome/NEC"
+indir="${basedir}/VAMB/Concate_bins"
+
+sample=`sed -n ${SLURM_ARRAY_TASK_ID}p ${indir}/concate_list.txt`
+
+
+set -x
+time busco -i ${indir}/${sample}.fasta -l bacteria_odb10 -o ${sample} -m genome -e 0.05
+RC=$?
+set +x
+
+if [ $RC -eq 0 ]
+then
+  echo "Job completed successfully"
+else
+  echo "Error Occured in ${sample}!"
+  exit $RC
+fi
---
+++ Bash scripts/s03b_busco_sum.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#===============================================================================
+#
+# File Name    : s03b_busco_sum.sh
+# Description  : Summarize busco data into a single file
+# Usage        : sbatch s03b_busco_sum.sh
+# Author       : Kailun Zhang, kailun@wustl.edu
+# Version      : 1.0
+# Created On   : Nov 05 2021
+# Modified On  : May 26 2022
+#===============================================================================
+
+basedir="/scratch/gdlab/kailun/Phageome/NEC"
+indir="${basedir}/BUSCO_concate"
+samplelist="${basedir}/VAMB/Concate_bins/concate_list.txt"
+outfile="${indir}/BUSCO_summaries/busco_sum.tsv"
+
+# create output file
+touch ${outfile}
+header="Assembly\t Complete (single)\t Complete (duplicated)\t Fragmented\t Missing"
+echo -e ${header} >> ${outfile}
+
+# loop through samplelist and append stats to outfile
+while read sample; do
+        # load data
+        assembly="${sample}"
+        single=`grep single-copy ${indir}/${sample}/short_summary.specific.bacteria_odb10.${sample}.txt | cut -c 2`
+        duplicate=`grep duplicated ${indir}/${sample}/short_summary.specific.bacteria_odb10.${sample}.txt | cut -c 2`
+        fregament=`grep Fragmented ${indir}/${sample}/short_summary.specific.bacteria_odb10.${sample}.txt | cut -c 2`
+        miss=`grep Missing ${indir}/${sample}/short_summary.specific.bacteria_odb10.${sample}.txt | cut -c 2-4`
+        echo -e "${assembly}\t${single}\t${duplicate}\t${fregament}\t${miss}" >> ${outfile}
+
+done < ${samplelist}
