ffa2d38c413453054dc80d81d8545f795a98ce00
KailunZM <89222331+KailunZM@users.noreply.github.com>
2026-02-05 21:13:25 -0600

    Add files via upload

---
+++ Bash scripts/s05_hmmsearch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#===============================================================================
+# File Name    : s04_hmmsearch.sh
+# Description  : This script will run amrfinder in parallel
+# Usage        : sbatch s04_hmmsearch.sh
+# Author       : Kailun Zhang, kailun@wustl.edu
+# Version      : 1.2
+# Created On   : Jul 22 2021 by Luke Diorio-Toth, ldiorio-toth@wustl.edu
+# Last Modified: Nov 03 2021
+#===============================================================================
+#Submission script for HTCF
+#SBATCH --job-name=HMMER
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=16
+#SBATCH --output=/scratch/gdlab/kailun/Phageome/NEC/slurm/hmmsearch/x_hmmsearch_%A_%a.out
+#SBATCH --error=/scratch/gdlab/kailun/Phageome/NEC/slurm/hmmsearch/y_hmmsearch_%A_%a.out
+
+module load hmmer
+
+basedir="/scratch/gdlab/kailun/Phageome/NEC"
+indir="${basedir}/HmmSearch"
+
+
+hmmsearch --tblout ${indir}/hmmsearch.out -E 0.05 ${indir}/final_list.hmms ${basedir}/CheckV_concate_pass4/tmp/proteins.faa
